6e19b9dbbb9c0c3e0d00b0279c0845db956f1cf5
wern ancheta <ancheta.wern@gmail.com>
2017-09-21 10:49:16 +0800

    initialize project

---
+++ app/index.android.js
@@ -0,0 +1,374 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Platform,
+  PermissionsAndroid,
+  StyleSheet,
+  Text,
+  View,
+  NativeAppEventEmitter,
+  NativeEventEmitter,
+  NativeModules,
+  Button,
+  ToastAndroid,
+  FlatList,
+  Alert
+} from 'react-native';
+
+import BleManager from 'react-native-ble-manager';
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+
+import { stringToBytes } from 'convert-string';
+import RandomId from 'random-id';
+import bytesCounter from 'bytes-counter';
+import Pusher from 'pusher-js/react-native';
+import Spinner from 'react-native-spinkit';
+import Prompt from 'react-native-prompt';
+
+export default class pusherBLEAttendance extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      is_scanning: false,
+      peripherals: null,
+      connected_peripheral: null,
+      user_id: null,
+      attendees: null,
+      promptVisible: false,
+      has_attended: false
+    }
+
+    this.peripherals = [];
+
+    this.startScan = this.startScan.bind(this);
+    this.openBox = this.openBox.bind(this);
+  }
+
+
+  componentWillMount() {
+    BleManager.enableBluetooth()
+      .then(() => {
+        console.log('Bluetooth is already enabled');
+      })
+      .catch((error) => {
+        Alert.alert('You need to enable bluetooth to use this app.');
+      });
+    
+    BleManager.start({showAlert: false})
+    .then(() => {
+      console.log('Module initialized');
+    });
+
+    if(Platform.OS === 'android' && Platform.Version >= 23){
+      PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
+        if(!result){
+          PermissionsAndroid.requestPermission(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
+            if(!result){
+              Aler.alert('You need to give access to coarse location to use this app.');
+            }
+          });
+        }
+      });
+    }
+
+  }
+
+
+  componentDidMount() {
+    bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', (peripheral) => {
+      
+      var peripherals = this.peripherals;
+      var el = peripherals.filter((el) => {
+        return el.id === peripheral.id;
+      });
+
+      if(!el.length){
+        peripherals.push({
+          id: peripheral.id,
+          name: peripheral.name
+        });
+
+        this.peripherals = peripherals;
+      }
+    });
+
+    bleManagerEmitter.addListener(
+      'BleManagerStopScan',
+      () => {
+        console.log('scan stopped');
+        if(this.peripherals.length == 0){
+          Alert.alert('Nothing found', "Sorry, no peripherals we're found");
+        }
+        this.setState({
+          is_scanning: false,
+          peripherals: this.peripherals
+        });  
+      }
+    );
+
+    var pusher = new Pusher('YOUR PUSHER APP KEY', {
+      cluster: 'YOUR PUSHER APP CLUSTER',
+      encrypted: true
+    });
+
+    var channel = pusher.subscribe('attendance-channel');
+    channel.bind('attendance-event', (data) => {
+      if(data.length > 1){
+        this.setState({
+          attendees: data
+        });
+      }else{
+        ToastAndroid.show(`${data.full_name} just entered the room!`, ToastAndroid.LONG);
+        this.setState({
+          attendees: [...this.state.attendees, data]
+        });
+      }
+
+    });
+  }
+
+
+  startScan() {
+    this.peripherals = [];
+    this.setState({
+      is_scanning: true
+    });
+
+    BleManager.scan([], 2, true)
+    .then(() => { 
+      console.log('scan started');
+    });
+
+  }
+
+
+  connect(peripheral_id) {
+    BleManager.connect(peripheral_id)
+      .then(() => {
+        this.setState({
+          connected_peripheral: peripheral_id
+        });
+
+        Alert.alert('Connected!', 'You are now connected to the peripheral.');
+
+        BleManager.retrieveServices(peripheral_id)
+          .then((peripheralInfo) => {
+            console.log('Peripheral info:', peripheralInfo);
+          }
+        ); 
+      })
+      .catch((error) => {
+        Alert.alert("Err..", 'Something went wrong while trying to connect.');
+      });
+
+  }
+
+
+  attend(value) {
+   
+    let user_id = RandomId(15);
+
+    this.setState({
+      user_id: user_id
+    });
+
+    let me = {
+      id: user_id,
+      full_name: value
+    }; 
+
+    let str = JSON.stringify(me);
+    let bytes = bytesCounter.count(str);
+    let data = stringToBytes(str);
+
+    let service_id = '12ab';
+    let characteristic_id = '34cd';
+
+    BleManager.write(this.state.connected_peripheral, service_id, characteristic_id, data, bytes)
+      .then(() => {
+
+        this.setState({
+          has_attended: true
+        });
+
+        BleManager.disconnect(this.state.connected_peripheral)
+          .then(() => {
+            Alert.alert('Attended', 'You have successfully attended the event, please disable bluetooth.');
+          })
+          .catch((error) => {
+            Alert.alert('Error disconnecting', "You have successfully attended the event but there's a problem disconnecting to the peripheral, please disable bluetooth to force disconnection.");
+          });
+
+      })
+      .catch((error) => {
+        Alert.alert('Error attending', "Something went wrong while trying to attend. Please try again.");
+      });
+  }
+
+
+  renderItem({item}) {
+
+    if(item.full_name){
+      return (
+        <View style={styles.list_item} key={item.id}>
+          <Text style={styles.list_item_text}>{item.full_name}</Text>
+          <Text style={styles.list_item_text}>{item.time_entered}</Text>
+        </View>
+      );
+    }
+
+    return (
+      <View style={styles.list_item} key={item.id}>
+        <Text style={styles.list_item_text}>{item.name}</Text>
+        <Button 
+          title="Connect" 
+          color="#1491ee" 
+          style={styles.list_item_button} 
+          onPress={this.connect.bind(this, item.id)} />
+      </View>
+    );
+  }
+
+
+  openBox() {
+    this.setState({
+      promptVisible: true
+    });
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.app_title}>
+            <Text style={styles.header_text}>BLE-Pusher Attendance</Text>   
+          </View>
+          <View style={styles.header_button_container}>
+            {
+              !this.state.connected_peripheral &&
+              <Button 
+                title="Scan" 
+                color="#1491ee" 
+                onPress={this.startScan} />
+            }
+          </View>
+        </View>
+        
+        <View style={styles.body}>
+        
+          <Spinner 
+            size={50} 
+            type={"WanderingCubes"} 
+            color={"#6097FC"} 
+            isVisible={this.state.is_scanning} 
+            style={styles.spinner}
+          />
+          
+          {
+            !this.state.connected_peripheral &&
+            <FlatList
+              data={this.state.peripherals}
+              renderItem={this.renderItem.bind(this)}
+            />
+          }
+
+          {
+            this.state.attendees &&
+            <View style={styles.attendees_container}>
+              <Prompt
+                  title="Enter your full name"
+                  placeholder="e.g. Son Goku"
+                  visible={this.state.promptVisible}
+                  onCancel={() => {
+                    this.setState({
+                      promptVisible: false
+                    });
+                  } 
+                  }
+                  onSubmit={ (value) => {
+                    this.setState({
+                      promptVisible: false
+                    });
+                    this.attend.call(this, value);
+                  }
+                  }/>
+              {
+                !this.state.has_attended && 
+                <Button 
+                  title="Enter" 
+                  color="#1491ee" 
+                  onPress={this.openBox} />
+              }
+              <FlatList
+                data={this.state.attendees}
+                renderItem={this.renderItem.bind(this)}
+              />
+            </View>
+          }
+
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    flex: 1,
+    backgroundColor: '#3B3738',
+    flexDirection: 'row'
+  },
+  app_title: {
+    flex: 7,
+    padding: 10
+  },
+  header_button_container: {
+    flex: 2,
+    justifyContent: 'center',
+    paddingRight: 5
+  },  
+  header_text: {
+    fontSize: 20,
+    color: '#FFF',
+    fontWeight: 'bold'
+  },
+  body: {
+    flex: 19
+  },
+  list_item: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    marginBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    flex: 1,
+    flexDirection: 'row'
+  },
+  list_item_text: {
+    flex: 8,
+    color: '#575757',
+    fontSize: 18
+  },
+  list_item_button: {
+    flex: 2
+  },
+  spinner: {
+    alignSelf: 'center',
+    marginTop: 30
+  },
+  attendees_container: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('pusherBLEAttendance', () => pusherBLEAttendance);
---
+++ server/attendance.js
@@ -0,0 +1,71 @@
+var bleno = require('bleno');
+var Pusher = require('pusher');
+
+var dateFormat = require('dateformat');
+
+require('dotenv').config();
+
+var pusher = new Pusher({
+  appId: process.env.APP_ID,
+  key: process.env.APP_KEY,
+  secret: process.env.APP_SECRET,
+  cluster: process.env.APP_CLUSTER,
+  encrypted: true
+});
+
+var time_format = 'h:MM TT';
+
+var attendees = [
+  {id: 1, full_name: 'milfa', time_entered: dateFormat(new Date(1505901033110), time_format)},
+  {id: 2, full_name: 'red', time_entered: dateFormat(new Date(1505901733110), time_format)},
+  {id: 3, full_name: 'silver', time_entered: dateFormat(new Date(1505908733110), time_format)}
+];
+
+var settings = {
+  service_id: '12ab',
+  characteristic_id: '34cd'
+};
+
+bleno.on('stateChange', function(state){
+  if(state === 'poweredOn'){
+    bleno.startAdvertising('AttendanceApp', [settings.service_id]);
+  }else{
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(error){
+    if(error){
+      console.log('something went wrong while trying to start advertisement of services');
+    }else{
+      console.log('started..');
+      bleno.setServices([
+        new bleno.PrimaryService({
+          uuid : settings.service_id,
+          characteristics : [
+            new bleno.Characteristic({
+              value : null,
+              uuid : settings.characteristic_id,
+              properties : ['write'],
+              onWriteRequest : function(data, offset, withoutResponse, callback){
+               
+                var attendee = JSON.parse(data.toString());
+                attendee.time_entered = dateFormat(new Date(), time_format);
+                attendees.push(attendee);
+                console.log(attendees);
+
+                pusher.trigger('attendance-channel', 'attendance-event', attendee);
+
+                callback(this.RESULT_SUCCESS);
+              }
+            })
+          ]
+        })
+      ]);
+    }
+});
+
+bleno.on('accept', function(clientAddress){
+  console.log('client address: ', clientAddress);
+  pusher.trigger('attendance-channel', 'attendance-event', attendees);
+});
